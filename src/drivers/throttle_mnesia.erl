f169768ef88402e3d8e144106f602b630d5f1501
Facundo Olano <facundo.olano@gmail.com>
2017-12-02 20:39:11 -0300

    mnesia working on multinode

--- src/drivers/throttle_mnesia.erl
+++ src/drivers/throttle_mnesia.erl
@@ -1,6 +1,7 @@
 -module(throttle_mnesia).
 
--export([init/0,
+-export([
+         init/0,
          init_counters/3,
          reset_counters/1,
          update_counter/2,
@@ -9,19 +10,27 @@
 
 -record(scope_state, {scope, limit, period, previous_reset}).
 
-%%% intialize the index table that tracks state for all the scopes
-%% FIXME this will require nodes to be connected first
-%% which means that maybe this step should be done manually and thus removed
-%% from regular app initialization
 init() ->
-  %% FIXME may need to start on all nodes?
-  %% rpc:multicall(Nodes, application, start, [mnesia]),
-  application:start(mnesia),
+  %% intially this will be called by the sup and every node will get its local
+  %% version of the tables. After connecting, call this manually so a single
+  %% copy of the table is shared across nodes.
 
-  {atomic, ok} = mnesia:create_table(scope_state,
-                                     [{attributes, record_info(fields, scope_state)},
-                                      {ram_copies, [node() | nodes()]},
-                                      {type, set}]),
+  AllNodes = [node() | nodes()],
+  OtherNodes = nodes(),
+
+  case application:start(mnesia) of
+    {error, {already_started, mnesia}} ->
+      %% not the first time, add new nodes
+      rpc:multicall(OtherNodes, application, stop, [mnesia]),
+      rpc:multicall(OtherNodes, application, start, [mnesia]),
+      mnesia:change_config(extra_db_nodes, AllNodes);
+    ok ->
+      %% first time, create state table
+      {atomic, ok} = mnesia:create_table(scope_state,
+                                         [{attributes, record_info(fields, scope_state)},
+                                          {ram_copies, AllNodes},
+                                          {type, set}])
+  end,
   ok.
 
 init_counters(Scope, Limit, Period) ->
@@ -52,7 +61,7 @@ update_counter(Scope, Key) ->
     [#scope_state{limit=Limit, period=Period, previous_reset=PreviousReset}] ->
       NextReset = throttle:interval(Period) - (timestamp() - PreviousReset),
 
-      %% FIXME should we wrap this in an activity and let the user configure what access context to use?
+      %% TODO should we wrap this in an activity and let the user configure what access context to use?
       Count = mnesia:dirty_update_counter(Scope, Key, 1),
       LimitedCount = min(Limit, Count),
 
@@ -77,6 +86,6 @@ lookup_counter(Scope, Key) ->
   end.
 
 %%% Internal functions
-%% TODO if always the same move to utils or throttle.erl
+%% FIXME if always the same move to utils or throttle.erl
 timestamp() ->
   erlang:system_time(millisecond).
