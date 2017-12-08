f0cbbd1a811b060d87d5cd29d0baaed8199b0853
Facundo Olano <facundo.olano@gmail.com>
2017-12-08 17:41:47 -0300

    test different access contexts for mnesia driver

--- test/throttle_distributed_SUITE.erl
+++ test/throttle_distributed_SUITE.erl
@@ -4,21 +4,40 @@
 
 -compile(export_all).
 
-%% FIXME test with async_dirty, transaction and sync_transaction
 groups() ->
-  [{distributed, [{repeat, 10}], [test_limit]}].
+  %% repeat to avoid "lucky" successes
+  [{async_dirty, [{repeat, 5}], [test_limit]},
+   {transaction, [{repeat, 5}], [test_limit]},
+   {sync_transaction, [{repeat, 5}], [test_limit]}
+  ].
 
 all() ->
-[{group, distributed}].
+  [{group, async_dirty},
+   {group, transaction},
+   {group, sync_transaction}].
 
 init_per_suite(Config) ->
   {ok, Slave} = start_slave(),
-  start_throttle(),
   [{slave, Slave} | Config].
 
 end_per_suite(Config) ->
   Slave = proplists:get_value(slave, Config),
   ct_slave:stop(Slave),
+  ok.
+
+init_per_group(AccessContext, Config) ->
+  Slave = proplists:get_value(slave, Config),
+  ok = rpc:call(Slave, throttle_distributed_SUITE, start_throttle, [AccessContext]),
+  start_throttle(AccessContext),
+
+  Sleep = case AccessContext of
+            %% async takes a bit to propagate the changes after the operation returns
+            async_dirty -> 50;
+            _ -> 0
+          end,
+  [{sleep, Sleep} | Config].
+
+end_per_group(_Context, Config) ->
   application:stop(throttle),
   ok.
 
@@ -29,28 +48,31 @@ start_slave() ->
   CodePath = code:get_path(),
   PathFlag = "-pa " ++ lists:concat(lists:join(" ", CodePath)),
   {ok, _} = ct_slave:start(Slave, [{erl_flags, PathFlag}]),
-
-  ok = rpc:call(Slave, throttle_distributed_SUITE, start_throttle, []),
   {ok, Slave}.
 
-start_throttle() ->
+start_throttle(Context) ->
   application:ensure_all_started(lager),
   ok = application:set_env(throttle, driver, throttle_mnesia),
-  ok = application:set_env(throttle, access_context, ets),
+  ok = application:set_env(throttle, access_context, Context),
   {ok, _Apps} = application:ensure_all_started(throttle),
   throttle_mnesia:init(),
   ok.
 
 test_limit(Config) ->
   Slave = proplists:get_value(slave, Config),
+  Sleep = proplists:get_value(sleep, Config),
   Scope = binary_to_atom(integer_to_binary(rand:uniform(10000)), latin1),
   throttle:setup(Scope, 4, per_second),
 
   %% check both here and in the other node
   {ok, 3, _} = throttle:check(Scope, <<"john">>),
+  timer:sleep(Sleep),
   {ok, 2, _} = rpc:call(Slave, throttle, check, [Scope, <<"john">>]),
+  timer:sleep(Sleep),
   {ok, 1, _} = throttle:check(Scope, <<"john">>),
+  timer:sleep(Sleep),
   {ok, 0, _} = rpc:call(Slave, throttle, check, [Scope, <<"john">>]),
+  timer:sleep(Sleep),
   {limit_exceeded, 0, _} = throttle:check(Scope, <<"john">>),
 
   timer:sleep(1100),
